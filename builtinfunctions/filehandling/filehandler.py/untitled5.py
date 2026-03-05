fa790c2e657167b4e36ee4cd2c3a51b59b49440c
Mendozaacpe <qamendoza25@tip.edu.ph>
2026-03-05 10:29:33 +0800

    Add files via upload

---
+++ builtinfunctions/filehandling/filehandler.py/untitled5.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  5 10:09:24 2026
+
+@author: TIPQC
+"""
+
+name = "Royce Chua"
+
+file = open("newfile1.txt", "w")
+file.write(f"Hello, {name}!\n")
+file.write("Isn't this amazing!\n")
+file.write("that we can create and write on text files\n")
+file.write("using Python.")
+file.close()
---
+++ builtinfunctions/filehandling/filereader.py/untitled8.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  5 10:24:54 2026
+
+@author: TIPQC
+"""
+
+file = open("new.tect" , 'r')
+data = file.read()
+print(data)
+file.close()
